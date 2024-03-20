c989360a07cab102d6602d0371491f5a5dd941d3
A Venkatesh <avenkateshanand1998@gmail.com>
2024-03-20 19:15:38 +0530

    Component Creations #4

---
+++ src/app/pages/question-count/question-count.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-count',
+  templateUrl: './question-count.component.html',
+  styleUrl: './question-count.component.css'
+})
+export class QuestionCountComponent {
+
+}
